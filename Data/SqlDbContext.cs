7be8aebcd764e266b8d7b9b582defdc56999a3ac
Wasiq Manzoor <164636793+Wasiqmanzoor90@users.noreply.github.com>
2025-01-31 11:19:47 +0530

    v1

---
+++ Data/SqlDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sql_Migration.Models;
+
+namespace Sql_Migration.Data
+{
+    public class SqlDbContext: DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options):base(options) 
+        {
+
+        }
+        public DbSet<User> User2 { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sql_Migration.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        [EmailAddress]
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public string? Role { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+    }
+}
